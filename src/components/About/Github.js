2f53188a8804952b8fae10e8db9cbbb4ce35f3c6
prangan27 <pranganghosh7@gmail.com>
2023-06-21 13:57:44 +0530

    Add files via upload

---
+++ src/components/About/Github.js
@@ -0,0 +1,25 @@
+import React from "react";
+import GitHubCalendar from "react-github-calendar";
+import { Row } from "react-bootstrap";
+import ReactTooltip from 'react-tooltip';
+
+function Github() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+      <h1 className="project-heading" style={{ paddingBottom: "20px" }}>
+        Days I <strong className="purple">Code</strong>
+      </h1>
+      <GitHubCalendar
+        username="soumyajit4419"
+        blockSize={15}
+        blockMargin={5}
+        color="#c084f5"
+        fontSize={16}
+      >
+      <ReactTooltip html />
+      </GitHubCalendar>
+    </Row>
+  );
+}
+
+export default Github;
