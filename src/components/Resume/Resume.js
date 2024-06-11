d0ba5de80c72bceb081a81e1768c428bf8c0acb9
prangan27 <pranganghosh7@gmail.com>
2024-06-11 17:05:37 +0530

    Update Resume.js

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -2,15 +2,15 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../resume/Prangan_Resume_2023.pdf";
+import pdf from "../../resume/ResumePranganGhosh.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js
-`;
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://github.com/prangan27/Portfolio/blob/main/src/Assets/Prangan_Resume_2023.pdf";
+  "https://github.com/prangan27/Portfolio/blob/main/src/resume/ResumePranganGhosh.pdf";
+
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
